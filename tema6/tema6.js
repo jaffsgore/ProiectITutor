9452f61a4a3d9bb8969032a70798c728e52aece8
Calin Tuta <tutacalin@gmail.com>
2020-02-24 00:03:48 +0200

    modificare structura teme in foldere

---
+++ tema6/tema6.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Buna seara!";
+  }
+  var person = {
+    firstName: "Marin",
+    lastName: "Ion",
+    age: 5,
+    eyeColor: "blue"
+  };
+  document.getElementById("jon").innerHTML =
+  person.firstName + person.lastName + " is " + person.age + " years old.";
