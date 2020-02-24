bb1dbe2441b004d8fb595ced07195aab15444097
Calin Tuta <60586750+jaffsgore@users.noreply.github.com>
2020-02-24 04:06:42 +0200

    Add files via upload

---
+++ tema7/js/tema7.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Acum!";
+   }
+   var orase = ["BAICOI", "CRAIOVA", "IASI", "SLOBOZIA"];
+document.getElementById("demo").innerHTML = orase;
+
+function myFunction() {
+  orase.push("CARACAL");
+  document.getElementById("demo").innerHTML = orase;
+}
