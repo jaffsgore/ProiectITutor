83ceb381d6d1b1d8041b44ce23eaba9b33c45267
Calin Tuta <60586750+jaffsgore@users.noreply.github.com>
2020-02-24 04:07:20 +0200

    Add files via upload

---
+++ tema7/php/tema7.php
@@ -0,0 +1,18 @@
+<?php
+function familyName($fname) {
+    echo "$fname Tuta.<br>";
+}
+
+familyName("Ana");
+familyName("Matei");
+familyName("Chuck");
+familyName("Trump");
+?>
+<?php
+$puncte = array("punct1", "punct2", "punct3");
+echo count($puncte);
+?>
+<?php
+$varsta = array("Ionut"=>"35", "Vasile"=>"37", "Tom"=>"43");
+echo "Tom is " . $age['Tom'] . " years old.";
+?>
