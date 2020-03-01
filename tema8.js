f140a7ebae58ff39250f028c4579152b014c998d
Calin Tuta <60586750+jaffsgore@users.noreply.github.com>
2020-03-02 01:19:33 +0200

    Add files via upload

---
+++ tema8.js
@@ -0,0 +1,10 @@
+        var c = document.getElementById("Canvas");
+        var ctx = c.getContext("2d");
+        ctx.moveTo(2,200);
+        ctx.lineTo(500,0);
+        ctx.stroke();
+        var d = new Date();
+d.setFullYear(2020);
+document.getElementById("demo").innerHTML = d;
+        
+        
---
+++ tema8.php
@@ -0,0 +1,3 @@
+
+Salut, <?php echo $_POST["name"]; ?><br>
+Adresa ta de email este: <?php echo $_POST["email"]; ?>
